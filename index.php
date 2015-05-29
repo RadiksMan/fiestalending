a7af73450a7e104bb6bb56b25c393de3d35e99ed
Radion <radion91@gmail.com>
2015-05-29 16:26:00 +0200

    no message

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?partial('section_1')?>
 <?partial('section_2')?>
 <?partial('section_3')?>
+<?partial('section_4')?>
+<?partial('section_5')?>
---
+++ partials/section_5.php
@@ -0,0 +1,40 @@
+<section class="section-5">
+    <div class="mbox">
+        <div class="section-5-wrap">
+            <div class="section-title text-center">
+                <span>РАЗНЫЕ КОСТЮМЫ ДЛЯ ВЫСТУПЛЕНИЙ</span>
+            </div>
+            <div class="section-5-middle">
+                <div class="section-5-middle-row cfix">
+                    <div class="section-5-middle-item-wrap">
+                        <a href="#">
+                            <div class="section-5-middle-item">
+
+                            </div>
+                        </a>
+                    </div>
+                    <div class="section-5-middle-item-wrap">
+                        <a href="#">
+                            <div class="section-5-middle-item">
+
+                            </div>
+                        </a>
+                    </div>
+                    <div class="section-5-middle-item-wrap">
+                        <a href="#">
+                            <div class="section-5-middle-item">
+
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="section-5-bottom text-center">
+                <p>Нас не только приятно слушать, на нас приятно смотреть</p>
+                <p>Группа «Fiesta» - это 5 стильных молодых парней и красивая девушка.</p>
+                <p>Мы трепетно относимся к своему внешнему виду, поэтому на сцене выглядим на  100 баллов.</p>
+            </div>
+
+        </div>
+    </div>
+</section>
