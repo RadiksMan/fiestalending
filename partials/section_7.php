76884520c4f4f54c923d75d9e216973c8398cc73
Radion Ponomarenko <radion91@gmail.com>
2015-05-30 13:13:13 +0300

    7 sect

--- partials/section_7.php
+++ partials/section_7.php
@@ -4,6 +4,34 @@
             <div class="section-title text-center white-text">
                 <span>Группа, которую рекомендуют звезды</span>
             </div>
+            <div class="section-7-middle cfix">
+                <div class="section-7-leftside">
+                    <div class="section-7-text">
+                        <p><span>Дмитрий Хрусталев</span></p>
+                        <p>ведущий первого канала</p>
+                        <div class="section-7-italic">
+                            <p>Я имел удавольствие работать</p>
+                            <p>с группой Fiesta</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-7-rightside text-right">
+                    <div class="section-7-text">
+                        <p><span>Виктор Васильев</span></p>
+                        <p>резидент Comedy Club</p>
+                        <div class="section-7-italic">
+                            <p>Отличные музыканты у которых хороший музыкальный</p>
+                            <p>вкус и мне завидно тем кто приглашает их</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-7-centerside text-center">
+                    <p>+7(967)366-77-36</p>
+                    <a href="#">
+                        <span>пригласить группу</span>
+                    </a>
+                </div>
+            </div>
 
 
         </div>
