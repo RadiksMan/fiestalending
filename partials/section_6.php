bc7557ce4e9304062599610668cd0aff864423e8
Radion Ponomarenko <radion91@gmail.com>
2015-05-30 11:46:21 +0300

    эй Дмитрий!

---
+++ partials/section_6.php
@@ -0,0 +1,32 @@
+<section class="section-6">
+    <div class="mbox">
+        <div class="section-6-wrap">
+            <div class="section-title text-center">
+                <span>Подбираем и разучиваем репертуар<br>индивидуально для каждого</span>
+            </div>
+            <div class="section-6-top-text text-center">
+                <p>У Вас есть возможность  услышать  песни, которые нравятся именно Вам… </p>
+                <p>Мы вместе с Вами подбираем и разрабатываем список</p>
+                <p>песен, которые будем исполнять  на празднике для Вас и ваших гостей.</p>
+            </div>
+
+            <div class="section-6-bottom-row cfix">
+                <div class="section-6-item text-right">
+                    <div class="section-6-button ">
+                        <a href="#">
+                            Узнать<br>
+                            подробнее
+                        </a>
+                    </div>
+                </div>
+                <div class="section-6-item text-center"><img src="images/doctor-who.png" alt=""></div>
+                <div class="section-6-item text-left">
+                    <div class="section-6-button ">
+                        <a href="tel:+79673667736">+7(967)366-77-36</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
